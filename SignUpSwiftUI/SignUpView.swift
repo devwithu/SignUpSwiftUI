6bb966960b8d08ef4f413698b544e1acfe016bbd
devwithu <devwithu@naver.com>
2020-01-31 20:27:31 +0900

    init

---
+++ SignUpSwiftUI/SignUpView.swift
@@ -0,0 +1,120 @@
+//
+//  ContentView.swift
+//  SignUpSwiftUI
+//
+//  Created by Frank Bara on 12/26/19.
+//  Copyright © 2019 BaraLabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    
+    @ObservedObject private var viewModel: SignUpViewModel
+   
+    init(viewModel: SignUpViewModel) {
+        self.viewModel = viewModel
+    }
+
+    var body: some View {
+        
+        ZStack {
+            ColorCodes.primary.color().edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Green Grocery")
+                    .font(Font.custom("Noteworthy-Bold", size: 40.0))
+                    .foregroundColor(.white)
+                    .padding(.bottom, 20)
+                
+                AuthTextField(textValue: $viewModel.username, title: "Username", errorValue:viewModel.usernameError)
+                AuthTextField(textValue: $viewModel.email, title: "Email Address", errorValue:viewModel.emailError, keyboardType: .emailAddress)
+                AuthTextField(textValue: $viewModel.password, title: "Password", errorValue:viewModel.passwordError, isSecured: true)
+                AuthTextField(textValue: $viewModel.confirmPassword, title: "Confirm Password", errorValue:viewModel.confirmPasswordError, isSecured: true)
+
+                Button(action: signUp) {
+                    Text("Sign Up")
+                }
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.black)
+                .cornerRadius(.infinity)
+                .padding(.top, 20)
+                
+                Text(viewModel.statusViewModel.title)
+                    //.font(headline)
+                    .fontWeight(.light)
+                    .padding(.top, 20)
+                    .foregroundColor(viewModel.statusViewModel.color.color())
+
+            }.padding(60.0)
+
+        }
+    }
+    
+    func signUp() -> Void {
+        print("Sign up tapped!")
+        
+        viewModel.signUp()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = SignUpViewModel()
+        return SignUpView(viewModel: viewModel)
+    }
+}
+
+extension ColorCodes {
+    
+    func color() -> Color {
+        switch self {
+        case .primary:
+            return Color(red: 79/255, green: 139/255, blue: 43/255)
+        case .success:
+            return Color(red: 0, green: 0, blue: 0)
+        case .failure:
+            return Color(red: 219/255, green: 12/255, blue: 12/255)
+        case .background:
+            return Color(red: 239/255, green: 243/255, blue: 244/255, opacity: 1.0)
+            
+        }
+    }
+}
+
+struct AuthTextField: View {
+    @Binding var textValue: String
+    
+    var title: String
+    var errorValue: String
+    var isSecured: Bool = false
+    var keyboardType: UIKeyboardType = .default
+    
+    var body: some View {
+        
+        VStack {
+            if isSecured {
+                // Show secure field for entering password
+                SecureField(title, text: $textValue)
+                    .padding()
+                    .background(ColorCodes.background.color())
+                    .cornerRadius(5)
+                    .keyboardType(keyboardType)
+            } else {
+                
+                TextField(title, text: $textValue)
+                    .padding()
+                    .background(ColorCodes.background.color())
+                    .cornerRadius(5)
+                    .keyboardType(keyboardType)
+            }
+                
+                Text(errorValue)
+                    .fontWeight(.light)
+                    .foregroundColor(ColorCodes.failure.color())
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+        }
+    }
+}
---
+++ ViewModels/SignUpViewModel.swift
@@ -0,0 +1,211 @@
+//
+//  SignUpViewModel.swift
+//  SignUpSwiftUI
+//
+//  Created by Frank Bara on 12/27/19.
+//  Copyright © 2019 BaraLabs. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class StatusViewModel: ObservableObject {
+    var title: String
+    var color: ColorCodes
+    
+    init(title: String, color: ColorCodes) {
+        self.title = title
+        self.color = color
+    }
+}
+
+class SignUpViewModel: ObservableObject {
+    
+    //private let authApi: AuthAPI
+    //private let authServiceParser: AuthServiceParser
+    
+    private var cancellableBag = Set<AnyCancellable>()
+    
+    @Published var username: String = ""
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var confirmPassword: String = ""
+    
+    @Published var statusViewModel: StatusViewModel = StatusViewModel(title: "", color: .success)
+    
+    var usernameError: String = ""
+    var emailError: String = ""
+    var passwordError: String = ""
+    var confirmPasswordError: String = ""
+    
+    private var usernameValidPublisher: AnyPublisher<Bool, Never> {
+        
+        return $username
+            .map { !$0.isEmpty }
+            .eraseToAnyPublisher()
+    }
+    
+    private var emailRequiredPublisher: AnyPublisher<(email: String, isValid: Bool), Never> {
+        
+        return $email
+            .map { (email: $0, isValid: !$0.isEmpty) }
+            .eraseToAnyPublisher()
+    }
+    
+    private var emailValidPublisher: AnyPublisher<(email: String, isValid: Bool), Never> {
+        
+        return emailRequiredPublisher
+            .filter { $0.isValid }
+            .map { (email: $0.email, isValid: $0.email.isValidEmail()) }
+            .eraseToAnyPublisher()
+    }
+    
+    private var passwordRequiredPublisher: AnyPublisher<(password: String, isValid: Bool), Never> {
+        
+        return $password
+            .map { (password: $0, isValid: !$0.isEmpty) }
+            .eraseToAnyPublisher()
+    }
+    
+    private var passwordValidPublisher: AnyPublisher<Bool, Never> {
+        
+        return passwordRequiredPublisher
+            .filter { $0.isValid }
+            .map { $0.password.isValidPassword() }
+            .eraseToAnyPublisher()
+    }
+    
+    private var confirmPasswordRequiredPublisher: AnyPublisher<(password: String, isValid: Bool), Never> {
+        
+        return $confirmPassword
+            .map { (password: $0, isValid: !$0.isEmpty) }
+            .eraseToAnyPublisher()
+    }
+    
+    private var passwordEqualPublisher: AnyPublisher<Bool, Never> {
+        
+        return Publishers.CombineLatest($password, $confirmPassword)
+            .filter { !$0.0.isEmpty && !$0.1.isEmpty }
+            .map { password, confirm in
+                return password == confirm
+            }
+        .eraseToAnyPublisher()
+    }
+
+    
+    init() {
+        
+        usernameValidPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0 ? "" : "Username is missing" }
+            .assign(to: \.usernameError, on: self)
+            .store(in: &cancellableBag)
+        
+        emailRequiredPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0.isValid ? "" : "Email is missing." }
+            .assign(to: \.emailError, on: self)
+            .store(in: &cancellableBag)
+        
+        emailValidPublisher
+            .receive(on: RunLoop.main)
+            .map { $0.isValid ? "" : "Email is not valid." }
+            .assign(to: \.emailError, on: self)
+            .store(in: &cancellableBag)
+        
+        passwordRequiredPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0.isValid ? "" : "Password is missing."}
+            .assign(to: \.passwordError, on: self)
+            .store(in: &cancellableBag)
+        
+        passwordValidPublisher
+            .receive(on: RunLoop.main)
+            .map { $0 ? "" : "Password must be 8 characters with 1 alpa and 1 number" }
+            .assign(to: \.passwordError, on: self)
+            .store(in: &cancellableBag)
+        
+        confirmPasswordRequiredPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0.isValid ? "" : "Confirm password is missing."}
+            .assign(to: \.confirmPasswordError, on: self)
+            .store(in: &cancellableBag)
+        
+        passwordEqualPublisher
+            .receive(on: RunLoop.main)
+            .dropFirst()
+            .map { $0 ? "" : "Password does not match."}
+            .assign(to: \.confirmPasswordError, on: self)
+            .store(in: &cancellableBag)
+        
+//        Publishers.CombineLatest4(usernameValidPublisher,
+//                                  emailValidPublisher,
+//                                  passwordValidPublisher,
+//                                  passwordEqualPublisher)
+//            .map {
+//                username, email, password, confirm in
+//                return username && email && password && confirm
+//        }
+//        .receive(on: RunLoop.main)
+//        .assign(to: \.enableSignUp, on: self)
+//        .store(in: &cancellableBag)
+    }
+    
+    deinit {
+        cancellableBag.removeAll()
+    }
+}
+
+extension SignUpViewModel {
+
+    func signUp() -> Void{
+        print("signUp")
+        
+//        authApi.signUp(username: username, email: email, password: password)
+//            .flatMap { [authServiceParser] in
+//                authServiceParser.parseSignUpResponse(statusCode: $0.statusCode, data: $0.data)
+//        }
+//        .map { result in
+//            switch(result) {
+//            case .success:
+//                return StatusViewModel(title: "Sign Up is successful", color: ColorCodes.success)
+//                break
+//            case .failure:
+//                return StatusViewModel(title: "Sign Up is failed", color: ColorCodes.failure)
+//                break
+//            }
+//        }
+//        .receive(on: RunLoop.main)
+//        .replaceError(with: StatusViewModel(title: "Sign Up is failed", color: ColorCodes.failure))
+//        .handleEvents(receiveOutput: {
+//            [weak self] _ in
+//            self?.username = ""
+//            self?.email = ""
+//            self?.password = ""
+//            self?.confirmPassword = ""
+//        })
+//            .assign(to: \.statusViewModel, on: self)
+//            .store(in: &cancellableBag)
+        
+        
+    }
+}
+
+extension String {
+    // Evaluates what the user entered to ensure it's a valid email address
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+_]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        
+        return emailPred.evaluate(with: self)
+    }
+    
+    func isValidPassword(pattern: String = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$") -> Bool {
+        let passwordRegex = pattern
+        return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self)
+    }
+}
